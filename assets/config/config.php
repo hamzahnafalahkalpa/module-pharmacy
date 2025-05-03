1dee3e27afd901ea1064604c4fcd2a0e9dab73c1
Hamzah <hamzahnafalah@gmail.com>
2025-05-03 12:43:40 +0700

    update modules

--- assets/config/config.php
+++ assets/config/config.php
@@ -1,34 +1,35 @@
 <?php
 
 use Hanafalah\ModulePharmacy\{
-    Models as ModulePharmacyModels,
-    Commands as ModulePharmacyCommands,
-    Schemas
+    Commands,
 };
-use Hanafalah\ModulePharmacy\Contracts;
 
 return [
-    'commands' => [
-        // ModulePharmacyCommands\InstallMakeCommand::class
+    'namespace' => 'Hanafalah\\ModulePharmacy',
+    'app' => [
+        'contracts' => [
+            //ADD YOUR CONTRACTS HERE
+        ]
+    ],
+    'libs' => [
+        'model' => 'Models',
+        'contract' => 'Contracts',
+        'schema' => 'Schemas',
+        'database' => 'Database',
+        'data' => 'Data',
+        'resource' => 'Resources',
+        'migration' => '../assets/database/migrations'
     ],
     "encodings" => [
         'PHARMACY_SALE' => ['flag' => 'PHARMACY_SALE', 'name' => 'Kode Penjualan Resep Obat/BMHP'],
     ],
-    'contracts' => [
-        'pharmacy_examination'             => Contracts\PharmacyExamination::class,
-        'pharmacy_sale_examination'        => Contracts\PharmacySaleExamination::class,
-        'pharmacy_sale_visit_registration' => Contracts\PharmacySaleVisitRegistration::class
-    ],
     'database' => [
         'models' => [
-            'PharmacySale'                  => ModulePharmacyModels\PharmacySale\PharmacySale::class,
-            'PharmacySaleVisitRegistration' => ModulePharmacyModels\PharmacySale\PharmacySaleVisitRegistration::class,
-            'PharmacySaleExamination'       => ModulePharmacyModels\PharmacySale\Dispense\PharmacySaleExamination::class
         ]
     ],
     'examinations' => [
-        'PharmacySaleExamination' => [
-            'schema' => Schemas\Dispense\PharmacySaleExamination::class
-        ]
-    ]
+    ],
+    'commands' => [
+        Commands\InstallMakeCommand::class
+    ],
 ];
