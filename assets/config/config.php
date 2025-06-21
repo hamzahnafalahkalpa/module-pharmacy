59a6697b689c32b7ad18b1f756c91bdcf7a852fa
Hamzah <hamzahnafalah@gmail.com>
2025-06-22 00:43:40 +0700

    modules

--- assets/config/config.php
+++ assets/config/config.php
@@ -12,14 +12,13 @@ return [
         // ModulePharmacyCommands\InstallMakeCommand::class
     ],
     "encodings" => [
-        'PHARMACY_SALE' => ['flag' => 'PHARMACY_SALE', 'name' => 'Kode Penjualan Resep Obat/BMHP'],
+        'PHARMACY_SALE' => [
+            'flag' => 'PHARMACY_SALE', 'name' => 'Kode Penjualan Resep Obat/BMHP'
+        ],
     ],
     'app' => [
         'contracts' => [
             //ADD YOUR CONTRACTS HERE
-            'pharmacy_examination'             => Contracts\PharmacyExamination::class,
-            'pharmacy_sale_examination'        => Contracts\PharmacySaleExamination::class,
-            'pharmacy_sale_visit_registration' => Contracts\PharmacySaleVisitRegistration::class
         ],
     ],
     'libs' => [
@@ -28,14 +27,8 @@ return [
     ],
     'database' => [
         'models' => [
-            'PharmacySale'                  => ModulePharmacyModels\PharmacySale\PharmacySale::class,
-            'PharmacySaleVisitRegistration' => ModulePharmacyModels\PharmacySale\PharmacySaleVisitRegistration::class,
-            'PharmacySaleExamination'       => ModulePharmacyModels\PharmacySale\Dispense\PharmacySaleExamination::class
         ]
     ],
     'examinations' => [
-        'PharmacySaleExamination' => [
-            'schema' => Schemas\Dispense\PharmacySaleExamination::class
-        ]
     ]
 ];
